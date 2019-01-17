bbb06a401a7f073ccd6ef998b9915c243a28df7c
Karoly Valentin <valentin.karoly@gmail.com>
2019-01-17 16:19:05 +0100

    adding printBigger

---
+++ greenfox/Valentin900614/week-01/day-4/src/PrintBigger.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PrintBigger {
+    public static void main(String[] args) {
+        // Write a program that asks for two numbers and prints the bigger one
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gimme the first number!");
+        int a = scanner.nextInt();
+
+        System.out.println("Gimme the second number!");
+        int b = scanner.nextInt();
+
+        if(a>b) {
+            System.out.println(a + " is bigger.");
+        }
+        if (a<b) {
+            System.out.println(b + " is bigger.");
+        }
+
+    }
+}
