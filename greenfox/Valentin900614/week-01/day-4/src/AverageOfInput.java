c64e10f2211faf2dde1c0281fb9d30731b4d0f98
Karoly Valentin <valentin.karoly@gmail.com>
2019-01-17 15:53:20 +0100

    adding averageOfInput

---
+++ greenfox/Valentin900614/week-01/day-4/src/AverageOfInput.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class AverageOfInput {
+    public static void main(String[] args) {
+        // Write a program that asks for 5 integers in a row,
+        // then it should print the sum and the average of these numbers like:
+        //
+        // Sum: 22, Average: 4.4
+        System.out.println("Gimme the first number!");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        System.out.println("Gimme the second number!");
+        int b = scanner.nextInt();
+
+        System.out.println("Gimme the third number!");
+        int c = scanner.nextInt();
+
+        System.out.println("Gimme the fourth number!");
+        int d = scanner.nextInt();
+
+        System.out.println("Gimme the fifth number!");
+        int e = scanner.nextInt();
+
+        int sum = a+b+c+d+e;
+
+        System.out.println("The sum of your numbers is: " + sum + ". Their average is: " + (a+b+c+d+e)/5 + ".");
+    }
+}
