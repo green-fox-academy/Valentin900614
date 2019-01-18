abffa7d70249d66cec7756427e41315ce51383b1
Karoly Valentin <valentin.karoly@gmail.com>
2019-01-18 15:28:13 +0100

    modified PrintEven

--- greenfox/Valentin900614/week-01/day-4/src/PrintEven.java
+++ greenfox/Valentin900614/week-01/day-4/src/PrintEven.java
@@ -1,7 +1,7 @@
 public class PrintEven {
     public static void main(String[] args) {
         // Create a program that prints all the even numbers between 0 and 500
-        for(int i=0; i<=500; i++){
+        for(int i=1; i<=500; i++){
             if(i%2==0){
                 System.out.println(i);
             }
