d302aa35c799bea508c70608e9feb6f7d3d47fda
Karoly Valentin <valentin.karoly@gmail.com>
2019-01-18 15:03:46 +0100

    modified MileToKm

--- greenfox/Valentin900614/week-01/day-4/src/MileToKm.java
+++ greenfox/Valentin900614/week-01/day-4/src/MileToKm.java
@@ -4,11 +4,11 @@ public class MileToKm {
         public static void main(String[] args) {
             // Write a program that asks for an integer that is a distance in kilometers,
             // then it converts that value to miles and prints it
-            System.out.println("Gimme a distance in km!");
             Scanner scanner = new Scanner(System.in);
+            System.out.println("Gimme a distance in km!");
             int a = scanner.nextInt();
             double b;
-            b = 1.6*a;
+            b = 1.609344*a;
             System.out.println("The gives distance is " + b + " in miles.");
         }
 }
