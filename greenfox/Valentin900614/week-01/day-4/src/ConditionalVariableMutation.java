9ef111605cb923dc3c8a473d4a9219ccfcabb918
Karoly Valentin <valentin.karoly@gmail.com>
2019-01-18 14:53:03 +0100

    modified ConditionalVariableMutation

--- greenfox/Valentin900614/week-01/day-4/src/ConditionalVariableMutation.java
+++ greenfox/Valentin900614/week-01/day-4/src/ConditionalVariableMutation.java
@@ -17,7 +17,7 @@ public class ConditionalVariableMutation {
         // if b is between 10 and 20 set out2 to "Sweet!"
         // if less than 10 set out2 to "Less!",
         // if more than 20 set out2 to "More!"
-        if(b>10 || b<20){
+        if(b>10 && b<20){
             out2 = "Sweet!";
         } else if (b<10) {
             out2 = "Less!";
