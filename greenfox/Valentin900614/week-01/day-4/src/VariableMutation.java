6b02403849b828b59358b9fc4663b804018e0699
Karoly Valentin <valentin.karoly@gmail.com>
2019-01-18 15:17:24 +0100

    modified VariableMutation

--- greenfox/Valentin900614/week-01/day-4/src/VariableMutation.java
+++ greenfox/Valentin900614/week-01/day-4/src/VariableMutation.java
@@ -22,57 +22,66 @@ public class VariableMutation {
 
         int e = 8;
         // please cube of e's value
-        e *= e;
-        e *= e;
+        e = e*e*e;
+
         System.out.println(e);
 
         int f1 = 123;
         int f2 = 345;
         // tell if f1 is bigger than f2 (print as a boolean)
+        boolean a1;
         if (f1>f2) {
-            System.out.println(true);
+            a1 = true;
         } else {
-            System.out.println(false);
+            a1 = false;
         }
+        System.out.println(a1);
 
         int g1 = 350;
         int g2 = 200;
         // tell if the double of g2 is bigger than g1 (print as a boolean)
+        boolean b1;
         if (2*g2>g1) {
-            System.out.println(true);
+            b1 = true;
         } else {
-            System.out.println(false);
+            b1 = false;
         }
+        System.out.println(b1);
 
         int h = 135798745;
         // tell if it has 11 as a divisor (print as a boolean)
+        boolean c1;
         if (h%11==0) {
-            System.out.println("Yes");
+            c1 = true;
         } else {
-            System.out.println("No");
+            c1 = false;
         }
+        System.out.println(c1);
 
         int i1 = 10;
         int i2 = 3;
         // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
+        boolean d1;
         if (i1 > i2*i2 && i1 < i2*i2*i2) {
-            System.out.println(true);
+            d1 = true;
         } else {
-            System.out.println(false);
+            d1 = false;
         }
+        System.out.println(d1);
 
         int j = 1521;
         // tell if j is dividable by 3 or 5 (print as a boolean)
+        boolean e1;
         if (j%3==0 && j%5==0) {
-            System.out.println(true);
+            e1 = true;
         } else {
-            System.out.println(false);
+            e1 = false;
         }
+        System.out.println(e1);
 
         String k = "Apple";
         //fill the k variable with its content 4 times
-        k += k;
-        k += k;
+        k = k+k+k+k;
         System.out.println(k);
     }
 }
