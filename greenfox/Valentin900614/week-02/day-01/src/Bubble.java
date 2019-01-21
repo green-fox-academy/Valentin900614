ef7279dc4b372d09f0d885477b5f2ac69b2dbb3e
Karoly Valentin <valentin.karoly@gmail.com>
2019-01-21 17:00:43 +0100

    adding Bubble

---
+++ greenfox/Valentin900614/week-02/day-01/src/Bubble.java
@@ -0,0 +1,53 @@
+public class Bubble {
+    public static void main(String[] args) {
+                //  Create a function that takes a list of numbers as parameter
+                //  Returns a list where the elements are sorted in ascending numerical order
+                //  Make a second boolean parameter, if it's `true` sort that list descending
+
+                //  Example:
+                System.out.println(bubble(new int[] {34, 12, 24, 9, 5}));
+
+                //  should print [5, 9, 12, 24, 34]
+                System.out.println(advancedBubble(new int[] {34, 12, 24, 9, 5}, true));
+                //  should print [34, 24, 12, 9, 5]
+    }
+
+    public static int[] bubble(int[] array){
+        int temp;
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (array[j] < array [j - 1]) {
+                    temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+        }
+      /*  for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }*/
+        return array;
+    }
+
+    public static int[] advancedBubble(int[] array, boolean ask) {
+        int temp;
+        if (ask == true) {
+            for (int i = 1; i < array.length; i++) {
+                for (int j = i; j > 0; j--) {
+                    if (array[j] > array[j - 1]) {
+                        temp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = temp;
+                    }
+                }
+            }
+      /*  for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }*/
+
+        }
+        return array;
+    }
+
+}
+
