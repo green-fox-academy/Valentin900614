7dd2e47016340a94c31d943018d8dfcae2c710fe
Karoly Valentin <valentin.karoly@gmail.com>
2019-01-22 16:09:46 +0100

    adding Reverse

---
+++ greenfox/Valentin900614/week-02/day-02/src/Reverse.java
@@ -0,0 +1,20 @@
+public class Reverse {
+    public static void main(String[] args){
+        String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+        // Create a method that can reverse a String, which is passed as the parameter
+        // Use it on this reversed string to check it!
+        // Try to solve this using charAt() first, and optionally anything else after.
+
+        System.out.println(reverse(reversed));
+    }
+    public static String reverse(String smthng) {
+        char help;
+        String uj = "";
+        for (int i = 0; i < smthng.length(); i++) {
+            help = smthng.charAt(smthng.length()-1-i);
+            uj += help;
+        }
+        return uj;
+    }
+}
