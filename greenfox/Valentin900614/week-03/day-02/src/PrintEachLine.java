8c15b7d8324c1da306bc0b2c1c9fa35ed590cea6
Karoly Valentin <valentin.karoly@gmail.com>
2019-01-29 13:04:26 +0100

    adding PrintEachLine

---
+++ greenfox/Valentin900614/week-03/day-02/src/PrintEachLine.java
@@ -0,0 +1,26 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintEachLine {
+    public static void main(String[] args) {
+        // Write a program that opens a file called "my-file.txt", then prints
+        // each line from the file.
+        // If the program is unable to read the file (for example it does not exist),
+        // then it should print the following error message: "Unable to read file: my-file.txt"
+
+        try {
+            Path path = Paths.get("my-file.txt");
+            List<String> list = Files.readAllLines(path);
+
+            for (int i = 0; i < ((List) list).size(); i++) {
+                System.out.println(list.get(i));
+            }
+        } catch (Exception ex) {
+            System.err.println("Unable to read file: my-file.txt");
+        }
+
+    }
+}
