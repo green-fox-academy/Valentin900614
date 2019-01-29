dea39e4a4496fad42fd716e4e70ea49d65c38c0f
Karoly Valentin <valentin.karoly@gmail.com>
2019-01-29 12:55:10 +0100

    adding DivideByZero

---
+++ greenfox/Valentin900614/week-03/day-02/src/DivideByZero.java
@@ -0,0 +1,19 @@
+public class DivideByZero {
+    public static void main(String[] args) {
+        // Create a function that takes a number
+        // divides ten with it,
+        // and prints the result.
+        // It should print "fail" if the parameter is 0
+
+        try {
+            calculate(0);
+        } catch (ArithmeticException ex) {
+            System.err.println("fail");
+        }
+    }
+
+    public static void calculate(int a) {
+        System.out.println(10/a);
+    }
+
+}
