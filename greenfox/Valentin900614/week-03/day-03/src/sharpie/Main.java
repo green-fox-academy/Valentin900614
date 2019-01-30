2f8cbbd16b39f4d924e4093f1ca222258fa8c584
Karoly Valentin <valentin.karoly@gmail.com>
2019-01-30 12:01:47 +0100

    adding sharpie

---
+++ greenfox/Valentin900614/week-03/day-03/src/sharpie/Main.java
@@ -0,0 +1,14 @@
+package sharpie;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+      /*  Sharpie sharpie = new Sharpie("BLACK", (float)1.2);
+        sharpie.use();
+        sharpie.use();
+        System.out.println(sharpie.color + " " + sharpie.width + " " + sharpie.inkAmount);
+*/
+    }
+}
---
+++ greenfox/Valentin900614/week-03/day-03/src/sharpie/Sharpie.java
@@ -0,0 +1,19 @@
+package sharpie;
+
+public class Sharpie {
+
+    String color;
+    Float width;
+    Float inkAmount;
+
+    public Sharpie(String color, Float width){
+        this.color = color;
+        this.width = width;
+        inkAmount = (float)100;
+    }
+
+    public void use(){
+        inkAmount--;
+    }
+
+}
