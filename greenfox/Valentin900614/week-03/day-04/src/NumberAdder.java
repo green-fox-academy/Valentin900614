65c6972107fbe765eeb422bb7af50eb03100be7d
Karoly Valentin <valentin.karoly@gmail.com>
2019-01-31 10:47:04 +0100

    adding NumberAdder

---
+++ greenfox/Valentin900614/week-03/day-04/src/NumberAdder.java
@@ -0,0 +1,17 @@
+public class NumberAdder {
+    public static void main(String[] args) {
+        // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+        System.out.println(adder(5));
+
+    }
+
+    public static int adder(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n + adder(n - 1);
+        }
+    }
+
+}
