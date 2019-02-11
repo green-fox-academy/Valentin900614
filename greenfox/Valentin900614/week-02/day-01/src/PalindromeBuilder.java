5c1f2b87a01b2ab2aa66afc128b628878da54ac6
Karoly Valentin <valentin.karoly@gmail.com>
2019-02-11 12:10:25 +0100

    adding BalindromeBuilder

---
+++ greenfox/Valentin900614/week-02/day-01/src/PalindromeBuilder.java
@@ -0,0 +1,15 @@
+public class PalindromeBuilder {
+    public static void main(String[] args) {
+        System.out.println(palindroming("Nemár!"));
+    }
+
+    public static String palindroming (String string) {
+        String backward = "";
+        for (int i = string.length() - 1; i >= 0; i--) {
+            backward += string.charAt(i);
+        }
+        string += backward;
+
+        return string;
+    }
+}
