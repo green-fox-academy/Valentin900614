c5fb5d68f753b24caeef1aebcca1ee9723b69295
Karoly Valentin <valentin.karoly@gmail.com>
2019-02-25 12:51:53 +0100

    adding exercise4

---
+++ greenfox/Valentin900614/week-06/day-01/src/exercise4.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class exercise4 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+
+        long numberOfAllOddNumbers = numbers.stream()
+                .filter(s -> s % 2 == 1 || s % 2 == -1)
+                .count();
+
+        long sumOfAllOddNumbers = numbers.stream()
+                .filter(s -> s % 2 == 1 || s % 2 == -1)
+                .mapToInt(i -> i).sum();
+
+        System.out.println(numberOfAllOddNumbers);
+        System.out.println(sumOfAllOddNumbers);
+        System.out.println((double)sumOfAllOddNumbers / (double)numberOfAllOddNumbers);
+
+    }
+}
