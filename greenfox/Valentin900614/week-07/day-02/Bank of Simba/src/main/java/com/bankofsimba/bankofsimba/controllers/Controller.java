e821abaaa9e5f760009fc3d704ccc01dfc3c6b14
Karoly Valentin <valentin.karoly@gmail.com>
2019-03-06 16:11:11 +0100

    adding bank of simba far from finished

---
+++ greenfox/Valentin900614/week-07/day-02/Bank of Simba/src/main/java/com/bankofsimba/bankofsimba/controllers/Controller.java
@@ -0,0 +1,37 @@
+package com.bankofsimba.bankofsimba.controllers;
+
+import com.bankofsimba.bankofsimba.classes.BankAccount;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    List<BankAccount> animalAccounts = new ArrayList<>();
+
+    public Controller() {
+
+        animalAccounts.add(new BankAccount("Simba", 2000, "lion"));
+        animalAccounts.add(new BankAccount("Nala", 1500, "lion"));
+        animalAccounts.add(new BankAccount("Timon", 800, "meerkat"));
+        animalAccounts.add(new BankAccount("Pumba", 650, "warthog"));
+        animalAccounts.add(new BankAccount("Zazu", 4238, "hornbill"));
+
+    }
+
+    @RequestMapping(value = "/show")
+    public String showAllAnimalAccounts (Model model) {
+        model.addAttribute("list", animalAccounts);
+        return "show";
+    }
+
+    @RequestMapping(value = "/string")
+    public String showAString (Model model) {
+        model.addAttribute("practiceString", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
+        return "string";
+    }
+
+}
