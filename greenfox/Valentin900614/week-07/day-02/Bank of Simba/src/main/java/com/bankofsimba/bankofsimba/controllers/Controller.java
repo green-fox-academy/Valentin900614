34a7ee0f35b2fb2d24ef7d900ae68e0535b8db50
Karoly Valentin <valentin.karoly@gmail.com>
2019-03-11 21:16:39 +0100

    simba with unknown error

--- greenfox/Valentin900614/week-07/day-02/Bank of Simba/src/main/java/com/bankofsimba/bankofsimba/controllers/Controller.java
+++ greenfox/Valentin900614/week-07/day-02/Bank of Simba/src/main/java/com/bankofsimba/bankofsimba/controllers/Controller.java
@@ -1,8 +1,11 @@
 package com.bankofsimba.bankofsimba.controllers;
 
-import com.bankofsimba.bankofsimba.classes.BankAccount;
+import com.bankofsimba.bankofsimba.model.BankAccount;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,18 +17,32 @@ public class Controller {
 
     public Controller() {
 
-        animalAccounts.add(new BankAccount("Simba", 2000, "lion"));
-        animalAccounts.add(new BankAccount("Nala", 1500, "lion"));
-        animalAccounts.add(new BankAccount("Timon", 800, "meerkat"));
-        animalAccounts.add(new BankAccount("Pumba", 650, "warthog"));
-        animalAccounts.add(new BankAccount("Zazu", 4238, "hornbill"));
+        animalAccounts.add(new BankAccount("Simba", 2000, "lion", true, true));
+        animalAccounts.add(new BankAccount("Nala", 1500, "lion", false, true));
+        animalAccounts.add(new BankAccount("Timon", 800, "meerkat", false, true));
+        animalAccounts.add(new BankAccount("Pumba", 650, "warthog", false, true));
+        animalAccounts.add(new BankAccount("Zazu", 4238, "hornbill", false, true));
 
     }
 
-    @RequestMapping("/show")
+    @RequestMapping(path = "/show", method = RequestMethod.GET)
     public String showAllAnimalAccounts (Model model) {
         model.addAttribute("list", animalAccounts);
+        model.addAttribute("newAccount", new BankAccount());
         return "show";
+    }
+
+    @RequestMapping(path = "/show", method = RequestMethod.POST)
+    public String raiseBalance(Model model, @RequestParam(name = "animalIndex") int index) {
+        animalAccounts.get(index).raiseBalance();
+        model.addAttribute("list", animalAccounts);
+        return "show";
+    }
+
+    @RequestMapping(path = "/show/add", method = RequestMethod.POST)
+    public String addNewAccount(@ModelAttribute BankAccount newAccount) {
+        animalAccounts.add(newAccount);
+        return "redirect:/show";
     }
 
     @RequestMapping(value = "/string")
--- greenfox/Valentin900614/week-07/day-02/Bank of Simba/src/main/java/com/bankofsimba/bankofsimba/model/BankAccount.java
+++ greenfox/Valentin900614/week-07/day-02/Bank of Simba/src/main/java/com/bankofsimba/bankofsimba/model/BankAccount.java
@@ -1,15 +1,23 @@
-package com.bankofsimba.bankofsimba.classes;
+package com.bankofsimba.bankofsimba.model;
 
 public class BankAccount {
 
     private String name;
     private int balance;
     private String animalType;
+    private boolean king;
+    private boolean goodGuy;
 
-    public BankAccount(String name, int balance, String animalType) {
+    public BankAccount() {
+
+    }
+
+    public BankAccount(String name, int balance, String animalType, boolean king, boolean goodGuy) {
         this.name = name;
         this.balance = balance;
         this.animalType = animalType;
+        this.king = king;
+        this.goodGuy = goodGuy;
     }
 
     public String getName() {
@@ -22,5 +30,41 @@ public class BankAccount {
 
     public String getAnimalType() {
         return animalType;
+    }
+
+    public boolean isKing() {
+        return king;
+    }
+
+    public boolean isGoodGuy() {
+        return goodGuy;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void setAnimalType(String animalType) {
+        this.animalType = animalType;
+    }
+
+    public void setKing(boolean king) {
+        this.king = king;
+    }
+
+    public void setGoodGuy(boolean goodGuy) {
+        this.goodGuy = goodGuy;
+    }
+
+    public void raiseBalance() {
+        if (this.isKing()) {
+            this.balance += 100;
+        } else {
+            this.balance += 10;
+        }
     }
 }
