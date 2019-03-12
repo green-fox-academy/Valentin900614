e0905adf86e4c6d5639512f8408a0fdeb728aaf5
Karoly Valentin <valentin.karoly@gmail.com>
2019-03-12 23:01:44 +0100

    modify

--- greenfox/Valentin900614/week-07/day-04/src/main/java/com/dependencyexamples/dependency/controller/UtilityService.java
+++ greenfox/Valentin900614/week-07/day-04/src/main/java/com/dependencyexamples/dependency/controller/UtilityService.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Controller
 @Service
@@ -39,5 +42,24 @@ public class UtilityService {
     public String colorizeTheBackground (Model model) {
         model.addAttribute("color", randomColor());
         return "useful";
+    }
+
+    @RequestMapping(path = "/useful/email", method = RequestMethod.GET)
+    public String checkDefaultEmail(@RequestParam(name = "email") String email, Model model) {
+        if (email != null) {
+            if (validateEmail(email)) {
+                model.addAttribute("answer", true);
+            } else
+                model.addAttribute("answer", false);
+        }
+
+        return "useful";
+    }
+
+    private boolean validateEmail(String string) {
+            Pattern pattern = Pattern.compile("^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$");
+            Matcher mat = pattern.matcher(string);
+
+            return mat.matches();
     }
 }
